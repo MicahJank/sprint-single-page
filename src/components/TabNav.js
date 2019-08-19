f50a674f05a5d170edc9a1bae9ad4c46d4bddf4f
MicahJank <MicahJank@users.noreply.github.com>
2019-08-18 20:15:47 -0500

    added in tab navigation using semantic ui

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,6 +3,17 @@ import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
+
+const panes = [
+    { menuItem: 'Home Page', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 4 Content</Tab.Pane> },
+  ]
+
 export default function TabNav() {
 
+    return (
+        <Tab panes={panes} />
+    );
 };
