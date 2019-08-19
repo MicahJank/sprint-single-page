bf752b952e3fdf28685d40a33069fcb061fe4f80
MicahJank <MicahJank@users.noreply.github.com>
2019-08-18 20:21:09 -0500

    imported the welcome page into the tabnav.js and used the tab to display the welcome page

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
+import WelcomePage from './WelcomePage.js';
+
+
+
 // TODO: Add missing tabs below
 
 const panes = [
-    { menuItem: 'Home Page', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Home Page', render: () => <Tab.Pane><WelcomePage /></Tab.Pane> },
     { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
     { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
     { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 4 Content</Tab.Pane> },
