9cf03b7fb7844fc1acc76942eaf4cbebfc453fc6
MicahJank <MicahJank@users.noreply.github.com>
2019-08-18 20:29:29 -0500

    began working on the characterlist component and created state and made an axios call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
 
+import CharacterCard from './CharacterCard.js';
+import Axios from "axios";
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get('https://rickandmortyapi.com/api/character/')
+      .then(apiObject => {
+        console.log(apiObject);
+        
+      })
+      .catch(err => {
+        alert(err);
+      });
+
   }, []);
 
-  return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+  if(!characters.length) {
+    return (<h2>Loading Characters...</h2>)
+  } else {
+    return (
+      <section className="character-list grid-view">
+        <h2>TODO: `array.map()` over your state here!</h2>
+      </section>
+    );
+  }
 }
+
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,6 +3,7 @@ import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 import WelcomePage from './WelcomePage.js';
+import CharacterList from './CharacterList.js';
 
 
 
@@ -10,7 +11,7 @@ import WelcomePage from './WelcomePage.js';
 
 const panes = [
     { menuItem: 'Home Page', render: () => <Tab.Pane><WelcomePage /></Tab.Pane> },
-    { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList /></Tab.Pane> },
     { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
     { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 4 Content</Tab.Pane> },
   ]
