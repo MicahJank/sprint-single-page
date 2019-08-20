02a2193bfd75c134003c6ec32f0cb43ebb91e02a
MicahJank <MicahJank@users.noreply.github.com>
2019-08-19 19:50:17 -0500

    styled the search bar a little

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from "react";
 import {Form, Field, withFormik} from 'formik';
 
+import styled from 'styled-components';
+
 import axios from 'axios';
 import { stat } from "fs";
+
+const SearchingForm = styled.section`
+  display: flex;
+  justify-content: center;
+  
+  input {
+    padding: 5px;
+    border: 2px solid #24325FFF;
+    margin-right: 5px;
+  }
+  button {
+    padding 7px;
+    background-color: #24325FFF;
+    border: none;
+    color: white;
+    cursor: pointer;
+  }
+`;
 
  function SearchForm({ errors, touched, values, status, setCharacters }) {
   // TODO: Add stateful logic for query/form data
@@ -19,12 +39,12 @@ import { stat } from "fs";
 
 
   return (
-    <section className="search-form">
+    <SearchingForm>
       <Form>
         <Field type='text' name='search' placeholder='Search characters...' />
         <button type='submit'>Search</button>
       </Form>
-    </section>
+    </SearchingForm>
   );
 }
 
